3ff6355f52b3a55cfe820139dfa963820abd6415
Gaspare Carollo <gasparecarollo@pursuit.org>
2024-01-23 11:20:28 -0500

    Added Footer, Header, Main, SearchNav

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+const Footer = () => {
+    return (
+        <Footer>
+            <p> Gaspare Carollo's onePage App</p>
+            <p> Created in 2024</p>
+        </Footer>
+    );
+}
+
+
+export default Footer;
---
+++ src/Components/Header.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+const Header = () => {
+
+    return (
+        <Header>
+            <img className='logo' src={lalala} alt='Place picture here' />
+        </Header>
+    );
+
+}
+
+export default Header;
