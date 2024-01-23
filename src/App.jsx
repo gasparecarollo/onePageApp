46bd2fc297f3de290878f1bbc4e5fa2846848f14
Gaspare Carollo <gasparecarollo@pursuit.org>
2024-01-23 12:48:10 -0500

    Set up App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import { useState } from 'react'
 import './App.css'
+import Header from './Components/Header.jsx'
+import SearchNav from './Components/SearchNav.jsx'
+import Main from './Components/Main.jsx'
+import Footer from './Components/Footer.jsx'
 
 function App() {
+  const [author, setAuthor] = useState(null)
 
   return (
-    <>
-      <div className='app'>
-      </div >
-    </>
+    <div className='app'>
+      <Header />
+      <SearchNav setAuthor={setAuthor} />
+      <Main author={author} />
+      <Footer />
+    </div >
   )
 }
 export default App
