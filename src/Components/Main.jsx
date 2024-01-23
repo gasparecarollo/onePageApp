236e678d53da9492776cce9bb433a9f7886d3286
Gaspare Carollo <gasparecarollo@pursuit.org>
2024-01-23 12:28:23 -0500

    Added fetch inside SearchNav

---
+++ src/Components/Main.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import WalterWhite from '../assets/WalterWhite/WalterWhite.png';
+
+const Main = ({ breakingbad }) => {
+    return (
+
+        <main className='breakingbad'>
+            {breakingbad ?
+                    <h2>
+
+                    </h2>
+                    <h3>
+
+                    </h3>
+                :
+
+                </div>
+}
+
+}
+
+        </main >
+
+    )
+}
+
+
+export default Main;
---
+++ src/Components/SearchNav.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import treadLightly from '../assets/treadLightly.png'
+
+const SearchNav = ({ setBreakingBad }) => {
+    const [quote, setQuote] = useState("")
+
+    const handleChange = (event) => {
+        setQuote((prev) => event.target.value)
+    }
+
+    const fetchQuote = (event) => {
+        event.preventDefault()
+        fetch(`https://api.breakingbadquotes.xyz/v1/${quote}`)
+            .then(res => res.json())
+            .then(res => {
+                setQuote(() => "")
+                setBreakingBad(res)
+            })
+    }
+    return (
+
+        <nav>
+            <form onSubmit={fetchQuote}>
+                <input
+                    type="text"
+                    onChange={handleChange}
+                    value={quote}
+                    placeholder="Search by Character Name"
+                />
+
+                <button type="submit">
+                    <img src={treadLightly}
+                        alt="Heisenberg tread lightly picture"
+                        className="Heisenberg-button"
+                    />
+                </button>
+            </form>
+        </nav>
+    );
+
+};
